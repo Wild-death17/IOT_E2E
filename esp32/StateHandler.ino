1d9b7d7002ba082e274129357ce97d8e6eb892fb
Wild_death17 <Moshe150519@Gmail.com>
2025-01-28 20:11:17 +0200

    setting up a stateHandler file

---
+++ esp32/StateHandler.ino
@@ -0,0 +1,30 @@
+//-----------------------------------
+  
+void StateSetup(){prevStateCheckTimeStamp = millis();}
+//-----------------------------------
+void HandleState(int value){
+  switch(value){
+
+        case LIGHT_MODE:
+
+            break;
+
+        case TEMP_MODE:
+
+            break;
+
+        case MOISTURE_MODE:
+
+            break;
+        case SHABAT_MODE:
+
+            break;
+        case MANUAL_MODE:
+
+            break;
+
+        default:
+            break;
+  }
+}
+//-----------------------------------
