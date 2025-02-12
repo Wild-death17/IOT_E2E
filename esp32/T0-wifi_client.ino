816898245a23ce95db7e51f36894221e11101cbf
Wild_death17 <Moshe150519@Gmail.com>
2025-02-12 09:21:18 +0200

    renamed removed space in naming

--- esp32/T0-wifi_client.ino
+++ esp32/T0-wifi_client.ino
@@ -1,26 +1,31 @@
 //-----------------------------------
-void WiFi_SETUP() {
+void WiFi_SETUP()
+{
   WiFi.begin(SSID);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
   Serial.println("Wifi conected");
 }
-void SEND_DATA(float temp, int moisture) {
+void SEND_DATA(float temp, int moisture)
+{
   HTTPClient http;
   String dataURL = "temp=" + String(temp);
   dataURL += "&moisture=" + String(moisture);
   http.begin(client, String(ADMIN) + ":" + String(PORT) + "/esp?" + dataURL);
   http.end();
 }
-int GET_STATE() {
+int GET_STATE()
+{
   HTTPClient http;
   int ret = -1;
   http.begin(client, String(ADMIN) + ":" + String(PORT) + "/esp/state");
   int httpCode = http.GET();
-  if (httpCode == HTTP_CODE_OK) {
+  if (httpCode == HTTP_CODE_OK)
+  {
     Serial.print("HTTP respond: ");
     String res = http.getString();
     ret = res.toInt();
@@ -28,13 +33,15 @@ int GET_STATE() {
   http.end();
   return ret;
 }
-String GET_DATA(String state) {
+String GET_DATA(String state)
+{
   HTTPClient http;
   String res = "";
   String currentState = "state=" + state;
   http.begin(client, String(ADMIN) + ":" + String(PORT) + "/esp/dataMode?" + currentState);
   int httpCode = http.GET();
-  if (httpCode == HTTP_CODE_OK) {
+  if (httpCode == HTTP_CODE_OK)
+  {
     Serial.print("HTTP respond: ");
     String res = http.getString();
     Serial.println(res);
