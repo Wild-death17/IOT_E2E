3810a4ddcf098f8d9ba2797e1d7112bc9a06c16d
Wild_death17 <Moshe150519@Gmail.com>
2025-02-18 22:20:49 +0200

    changed from screaming snake to pascal

--- esp32/T0-wifi_client.ino
+++ esp32/T0-wifi_client.ino
@@ -1,9 +1,7 @@
 //-----------------------------------
-void WiFi_SETUP()
-{
+void WifiSetup() {
   WiFi.begin(SSID);
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
@@ -11,22 +9,19 @@ void WiFi_SETUP()
   Serial.println("Wifi conected");
 }
 //-----------------------------------
-void SEND_DATA(float temp, int moisture)
-{
+void SendData(float temp, int moisture) {
   HTTPClient http;
   String dataURL = "temp=" + String(temp);
   dataURL += "&moisture=" + String(moisture);
   http.begin(client, String(ADMIN) + ":" + String(PORT) + "/esp?" + dataURL);
   http.end();
 }
-int GET_STATE()
-{
+int GetState() {
   HTTPClient http;
   int ret = -1;
   http.begin(client, String(ADMIN) + ":" + String(PORT) + "/esp/state");
   int httpCode = http.GET();
-  if (httpCode == HTTP_CODE_OK)
-  {
+  if (httpCode == HTTP_CODE_OK) {
     Serial.print("HTTP respond: ");
     String res = http.getString();
     ret = res.toInt();
@@ -34,16 +29,13 @@ int GET_STATE()
   http.end();
   return ret;
 }
-//-----------------------------------
-String GET_DATA(String state)
-{
+String GetData(String state) {
   HTTPClient http;
   String res = "";
   String currentState = "state=" + state;
   http.begin(client, String(ADMIN) + ":" + String(PORT) + "/esp/dataMode?" + currentState);
   int httpCode = http.GET();
-  if (httpCode == HTTP_CODE_OK)
-  {
+  if (httpCode == HTTP_CODE_OK) {
     Serial.print("HTTP respond: ");
     String res = http.getString();
     Serial.println(res);
